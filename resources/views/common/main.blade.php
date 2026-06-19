84c1277ea24dcbb969d471331454c91bdd9a59a9
LeDulan05 <phillipdylanpgarcia@gmail.com>
2026-06-19 19:47:07 +0800

    Initial commit

---
+++ resources/views/common/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Library System')</title>
+</head>
+<body>
+    
+</body>
+</html>
